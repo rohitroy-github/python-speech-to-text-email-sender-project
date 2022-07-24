81dc9de68cd4d45afbc32f75f29e8673522420cb
rohit <rohitdevops.roy@gmail.com>
2022-07-25 00:55:41 +0530

    Update ...

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 # Start
 
+# Dependencies
 from asyncio import SendfileNotAvailableError
 import speech_recognition as sr 
 import yagmail 
+
+# Encrypting the email address and password
+from decouple import config
+EMAIL_ADDRESS = config('emailAddress')
+EMAIL_PASSWORD = config('emailPassword')
 
 # creating an instance for speechRecognition 
 recognizer = sr.Recognizer() 
@@ -12,7 +18,7 @@ with sr.Microphone() as source:
     print('Clearing the background noise for clear recognition !')
     
     # Clearing background noise in input sound
-    recognizer.adjust_for_ambient_noise(source, duration=1)
+    recognizer.adjust_for_ambient_noise(source, duration=5)
     print('Waiting for your message >>> ')
     
     # Recording your audio
@@ -26,7 +32,7 @@ with sr.Microphone() as source:
         text = recognizer.recognize_google(recordedAudio, language='en-US')
         
         # Converting recorded audio into text > Language = English 
-        print('Your recorded message is :{}'.format(text))
+        print('Your recorded message is > {}'.format(text))
         
     except Exception as ex: 
         print(ex)
@@ -35,13 +41,13 @@ with sr.Microphone() as source:
 # Sending email        
 
 # Setting RECIEVER's email address
-reciever = 'rohitdevops.roy@gmail.com'
+reciever = EMAIL_ADDRESS
 
 #Setting the recorded text as email body
 message = text
 
 # Setting SENDER's email address
-sender = yagmail.SMTP('rohitsocial.roy@gmail.com')
+sender = yagmail.SMTP(EMAIL_ADDRESS, EMAIL_PASSWORD)
 
 # Sending the email now
 sender.send(to=reciever, subject='Voice assisted message !', contents=message)
